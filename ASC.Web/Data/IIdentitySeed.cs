0259e01e080b632f0d20ca98cb1cc7a1ebdc82d6
MinzzyStyp <stmin2k4@gmail.com>
2025-03-05 20:46:55 +0700

    lab2

---
+++ ASC.Web/Data/IIdentitySeed.cs
@@ -0,0 +1,12 @@
+﻿using ASC.Web.Configuration;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+namespace ASC.WEB.Data
+{
+    public interface IIdentitySeed
+    {
+        Task Seed(UserManager<IdentityUser> userManager,
+                  RoleManager<IdentityRole> roleManager,
+                  IOptions<ApplicationSettings> options);
+    }
+}
--- ASC.Web/Program.cs
+++ ASC.Web/Program.cs
@@ -1,9 +1,13 @@
+using ASC.DataAccess.Interface;
 using ASC.Solution.Services;
 using ASC.Web.Configuration;
 using ASC.Web.Data;
 using ASC.Web.Services;
+using ASC.WEB.Data;
+
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,16 +15,26 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.User.RequireUniqueEmail = true;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders();
+
+builder.Services.AddScoped<DbContext, ApplicationDbContext>();
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddOptions();
-builder.Services.Configure<ApplicationSettings>(builder.Configuration.GetSection("AppSettings"));
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages(); // Ensure Razor Pages services are added
+builder.Services.Configure<ApplicationSettings>(builder.Configuration.GetSection("AppSettings"));
+builder.Services.AddOptions();
 
 builder.Services.AddTransient<IEmailSender, AuthMessageSender>();
 builder.Services.AddTransient<ISmsSender, AuthMessageSender>();
+builder.Services.AddSingleton<IIdentitySeed, IdentitySeed>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -31,7 +45,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -40,11 +53,24 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication(); // Ensure authentication middleware is added
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
+
+// Run migrations automatically if needed
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var storageSeed = services.GetRequiredService<IIdentitySeed>();
+    await storageSeed.Seed(
+        services.GetRequiredService<UserManager<IdentityUser>>(),
+        services.GetRequiredService<RoleManager<IdentityRole>>(),
+        services.GetRequiredService<IOptions<ApplicationSettings>>()
+    );
+}
 
 app.Run();
